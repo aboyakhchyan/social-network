03b76b5d1567b6e2214bad7e33a054b2e1ec8ec9
aboyakhchyan <avoboyaxchyan@mail.ru>
2024-09-22 23:22:01 +0400

    first commit

---
+++ front/src/lib/api.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { ILogin, InputUser, IResponse } from "./types";
+
+const Axios = axios.create({
+    baseURL: "http://localhost:4002"
+})
+
+export const handleSignup = async (user: InputUser): Promise<IResponse> => {
+    const response = await Axios.post('/signup', user)
+
+    return response.data
+}
+
+export const handleLogin = async (user: ILogin): Promise<IResponse> => {
+    const response = await Axios.post('/login', user)
+
+    return response.data
+}
---
+++ front/src/pages/Signup/index.tsx
@@ -0,0 +1,123 @@
+import {
+    MDBContainer,
+    MDBCard,
+    MDBCardBody,
+    MDBCardImage,
+    MDBRow,
+    MDBCol,
+    MDBInput,
+}
+    from 'mdb-react-ui-kit';
+import { Link, useNavigate } from 'react-router-dom';
+import { InputUser } from '../../lib/types';
+import { handleSignup } from '../../lib/api';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useState } from 'react';
+
+const Schema = yup.object({
+    name: yup.string().required().min(4).max(10).test(
+        'is-first-letter-capital',
+        'First letter must be capitalized',
+        value => value ? /^[A-Z]/.test(value) : false
+    ),
+    surname: yup.string().required().min(6).max(15).test(
+        'is-first-letter-capital',
+        'First letter must be capitalized',
+        value => value ? /^[A-Z]/.test(value) : false
+    ),
+    login: yup.string().required().min(6).max(15),
+    password: yup.string().required().min(6).max(15)
+})
+
+export function Signup() {
+
+    const [error, setError] = useState<string>('')
+
+    const navigate = useNavigate()
+
+    
+    const {register, handleSubmit, formState: {errors}, reset} = useForm<InputUser>({
+        resolver: yupResolver(Schema)
+    })
+
+    const onSubmit = (data: InputUser): void => {
+        handleSignup(data)
+        .then(response => {
+            if(response.status == 'error' && response.message) {
+                setError(response.message)
+            }else {
+                navigate('/profile')
+                reset()
+            }
+        })
+    }
+
+    return (
+        <MDBContainer fluid>
+
+            <MDBRow className='d-flex justify-content-center align-items-center'>
+
+                <MDBCol lg='8'>
+
+                    <MDBCard className='my-5 rounded-3' style={{ maxWidth: '600px' }}>
+                        <MDBCardImage src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/img3.webp' className='w-100 rounded-top' alt="Sample photo" />
+
+                        <MDBCardBody className='px-5'>
+
+                            <h3 className="mb-4 pb-2 pb-md-0 mb-md-5 px-md-2">Registration Info</h3>
+                            <p>Already have an account? <Link to={'/login'}>Login Now</Link></p>
+
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                
+                                {error && <p style={{color: 'red'}}>{error}</p>}
+                                {errors.name && <p style={{color: 'red'}}>{errors.name.message}</p>}
+
+                                <MDBInput
+                                    wrapperClass='mb-4'
+                                    label='Name'
+                                    type='text'
+                                    {...register('name')}
+                                />
+
+                                {errors.surname && <p className='text-danger'>{errors.surname.message}</p>}
+
+                                <MDBInput
+                                    wrapperClass='mb-4'
+                                    label='Surname'
+                                    type='text'
+                                    {...register('surname')}
+                                />
+
+                                {errors.login && <p className='text-danger'>{errors.login.message}</p>}
+
+                                <MDBInput
+                                    wrapperClass='mb-4'
+                                    label='Login'
+                                    type='text'
+                                    {...register('login')}
+                                />
+
+                                {errors.password && <p className='text-danger'>{errors.password.message}</p>}
+
+                                <MDBInput
+                                    wrapperClass='mb-4'
+                                    label='Password'
+                                    type='password'
+                                    {...register('password')}
+                                />
+                                <button type='submit' className='btn btn-outline-info' >Submit</button>
+                            </form>
+
+
+
+                        </MDBCardBody>
+                    </MDBCard>
+
+                </MDBCol>
+            </MDBRow>
+
+        </MDBContainer>
+    );
+}
